024b4181d953033aefcb407e0b7bf70ed929c3f7
Nathan PintoAguila <nathan.pintoaguila.gamedesign@gmail.com>
2025-07-14 08:16:53 +0200

    Finalisation et ajout du style

--- script.js
+++ script.js
@@ -2,29 +2,61 @@ let cptTaches = 0
 
 let formCreerTache = document.querySelector("form")
 let saisieTache = document.getElementById("saisieTache")
+let btnSupprimerToutesTaches = document.querySelector(".zoneAffichageTachesEffectuees button")
 
-let zoneAffichageTachesAFaire = document.querySelector(".zoneAffichageTachesAFaire")
-
+// let tachesEffectuees = document.querySelector(".tachesEffectuees")
 
 formCreerTache.addEventListener("submit", (event) => {
     event.preventDefault()
-    console.log("Test")
     if (saisieTache.value !== "") {
         cptTaches++
         let divTache = `
             <div>
                 <input type="checkbox" name="taskToDo${cptTaches}" id="taskToDo${cptTaches}">
                 <label for="taskToDo${cptTaches}">${saisieTache.value}</label>
+                <button>Supprimer la tâche</button>
             </div>
         `
         let zoneAffichageTachesAFaire = document.querySelector(".zoneAffichageTachesAFaire")
         zoneAffichageTachesAFaire.insertAdjacentHTML("afterbegin", divTache)
     }
+    ajoutEventListenerTachesAFaire()
 })
 
-let listElementDansZoneAffichageTachesAFaire = document.querySelectorAll(".zoneAffichageTachesAFaire div")
 
-for (let i = 0 ; i < listElementDansZoneAffichageTachesAFaire ; i++) {
-    // Ajouter EventListener sur tous les inputs des taches to do pour permettre de détecter quand on coche la case pour déplacer la DIV.
-    //let checkBoxTacheToDo = document.query
+btnSupprimerToutesTaches.addEventListener("click", () => {
+    let listDivZoneAffichageTachesEffectuees = document.querySelectorAll(".zoneAffichageTachesEffectuees div")
+    for (let i = 0 ; i < listDivZoneAffichageTachesEffectuees.length ; i++) {
+        listDivZoneAffichageTachesEffectuees[i].remove()
+    }
+})
+//let listDivDansZoneAffichageTachesAFaire = document.querySelectorAll(".zoneAffichageTachesAFaire div")
+
+
+function ajoutEventListenerTachesAFaire() {
+    let listDivZoneAffichageTachesAFaire = document.querySelectorAll(".zoneAffichageTachesAFaire div")
+    let zoneAffichageTachesEffectuees = document.querySelector(".zoneAffichageTachesEffectuees")
+    let zoneAffichageTachesAFaire = document.querySelector(".zoneAffichageTachesAFaire")
+
+    for (let i = 0 ; i < listDivZoneAffichageTachesAFaire.length ; i++) {
+        // Ajouter EventListener sur tous les inputs des taches to do pour permettre de détecter 
+        // quand on coche la case pour déplacer la DIV.
+        
+        // !!! JE DOIS TROUVER COMMENT INSERER LES BALISES HTML QUI COMPOSENT MA VARIABLE tacheDone !!!
+        let checkBoxTacheToDo = listDivZoneAffichageTachesAFaire[i].querySelector("input")
+        let btnSupprimer = listDivZoneAffichageTachesAFaire[i].querySelector("button")
+        let tache = listDivZoneAffichageTachesAFaire[i]
+
+        checkBoxTacheToDo.addEventListener("click", () => {
+            if (checkBoxTacheToDo.checked) {
+                zoneAffichageTachesEffectuees.insertAdjacentElement("afterbegin", tache)
+            } else {
+                zoneAffichageTachesAFaire.insertAdjacentElement("afterbegin", tache)
+
+            }
+        })
+        btnSupprimer.addEventListener("click", () => {
+            tache.remove()
+        })
+    }
 }
